4094419e1adb68ec6e4a9c242a2b7edf189262fb
Bruno Alexandre Cavaco <brunoalx@hotmail.com>
2018-08-06 11:07:39 +0100

    I can see the reviews one by one

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -313,5 +313,5 @@ dbPromise.then(function(db){
   const tx = db.transaction('revs', 'readonly');
   const store = tx.objectStore('revs');
   const index = store.index('offlineDB');
-  return index.getAll().then(restaurant => console.log(restaurant));
+  return index.getAll().then(restaurant => restaurant.forEach(function(x){console.log(x)}));
 });
